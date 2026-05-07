8f5dc0eeb4a1cc6417082a7f5ad98816e5be4ad7
gonzalo367 <bm1306@est.ort.edu.ar>
2026-05-07 11:49:11 -0300

    Initial commit

---
+++ Assets/Script/CajaCollisionDeteccion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollisionDeteccion : MonoBehaviour
+{
+    public CajaMovement CajaMovementScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CajaMovementScript = GetComponent<CajaMovement>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        { 
+            CajaMovementScript.MoveBoxToRandomPosition();
+        }
+        }
+}
---
+++ Assets/Script/CajaMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+
+{
+    public float speed = 2f;
+    float initialYValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         initialYValue = transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,-speed * Time.deltaTime ,0);
+    }
+    public void MoveBoxToRandomPosition()
+    {
+    
+        //PARA PROBAR, SOLAMENTE HACEMOS QUE SUBA
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
+
+    }
+}
